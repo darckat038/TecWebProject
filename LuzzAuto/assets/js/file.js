6df7f615d0b2a326632c83d01cc71cf3a4e74611
FilippoDiviesti <filippo.diviesti@studenti.unipd.it>
2024-12-11 09:42:11 +0100

    add js file

---
+++ LuzzAuto/assets/js/file.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var x = document.getElementById("menu");
+    var y = document.getElementById("menu-button-container");
+
+    function adjustMenuOnResize() {
+        if (window.innerWidth >= 1024) {
+            x.style.display = "flex";                       // Passaggio a modalità desktop
+        } else {
+            if (x.style.display === "flex" && !(y.classList.contains("change"))) {
+                x.style.display = "none";                   // Passaggio da desktop a mobile
+            }
+                                                            // Passaggio da mobile a mobile
+        }
+    }
+
+    // Evento resize sulla finestra
+    window.addEventListener("resize", adjustMenuOnResize);
+});
+
+function menuOpenClose(y) {
+    var x = document.getElementById("menu");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+    y.classList.toggle("change");
+}
