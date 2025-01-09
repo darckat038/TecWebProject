de907fc7567474be3a1fa727554b8c85bed31a40
Filippo Righetto <righettofilippo.fr@gmail.com>
2025-01-09 21:45:04 +0100

    fixImmagine Utente

--- LuzzAuto/assets/js/file.js
+++ LuzzAuto/assets/js/file.js
@@ -26,3 +26,13 @@ function menuOpenClose(y) {
     }
     y.classList.toggle("change");
 }
+
+//saluto in base all'ora del giorno
+document.addEventListener("DOMContentLoaded", function(){
+    const oraCorrente = new Date().getHours();
+    const nomeUtente = "Giovanni";
+    const salutoUtente = document.getElementById("saluto");
+  
+    let saluto = oraCorrente < 18 ? "Buongiorno" : "Buonasera";
+    salutoUtente.textContent = `${saluto} ${nomeUtente}!`;
+  });
