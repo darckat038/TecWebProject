cddf3c2a65bd2499569e6974ca1707e6d4a967ee
EM123AR <emanueleartusi03@gmail.com>
2025-01-10 14:06:09 +0100

    Separazione toggle.js

---
+++ LuzzAuto/assets/js/toggle.js
@@ -0,0 +1,10 @@
+// Assicuriamo il funzionamento dell'espansione del filtro da mobile e la corretta gestione del aria-label
+function switchToggle() {
+    const toggleButton = document.getElementById('list_toggle_button');
+    const toggleInput = document.getElementById('list_toggle_filter');
+    const filterForm = document.getElementById('list_filter_form');
+
+    toggleInput.checked = !toggleInput.checked;
+    toggleButton.setAttribute('aria-expanded', toggleInput.checked.toString());
+    toggleButton.setAttribute('aria-label', toggleInput.checked ? 'Seleziona per comprimere il form filtro' : 'Seleziona per espandere il form filtro');
+}
