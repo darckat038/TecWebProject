64a848f0611bfe908f42f4209220bcd51ffe95a6
FilippoDiviesti <filippo.diviesti@studenti.unipd.it>
2025-01-14 11:01:48 +0100

    controllo errori

--- LuzzAuto/test_drive.php
+++ LuzzAuto/test_drive.php
@@ -3,7 +3,6 @@
 function ripristinoInput($test_driveHTML){
 	//RIPRISTINO DELL'INPUT INSERITO
     $a = $test_driveHTML;
-	$a = str_replace("[auto]", htmlspecialchars(isset($_POST['test_drive_select_auto']) ? $_POST['test_drive_select_auto'] : ''), $a);
 	$a = str_replace("[data]", htmlspecialchars(isset($_POST['test_drive_date']) ? $_POST['test_drive_date'] : ''), $a);
 	return $a;
 }
@@ -16,6 +15,7 @@ $test_driveHTML = file_get_contents('test_drive.html');
 $err = "";
 
 $formORbuttonsHTML = "";
+
 $formHTML = "
         
         <form id='test_drive_form' method='post'>
@@ -56,37 +56,38 @@ $buttonsHTML = "
 
 
 if(isset($_POST['test_drive_select_auto']) && isset($_POST['test_drive_date'])){
-    if(empty($_POST['test_drive_date']) || empty($_POST['test_drive_select_auto'])){
-        $formORbuttonsHTML = $formHTML;
-        $test_driveHTML = str_replace("[formORbuttons]", $formORbuttonsHTML, $test_driveHTML);
 
-        //ESECUZIONE DELLA QUERY
-        try{
-            $a = array();
-            $db = new DBConnection();
-            $ris = $db->getFilteredVehicles($a);
-            $db->closeConnection();
-            unset($db);
-            $veicoli = '';
-            //INSERIMENTO DEI VEICOLI NEL SELECT
-            foreach($ris as $row){
-                $val = $row["marca"] . "-" . $row['modello'];
-                //REIMPOSTO IL VALORE SETTATO
-                if($_POST['test_drive_select_auto'] == $val){
-                    $veicoli .= "<option value='" . $row["marca"] . "-" . $row['modello'] . "' selected>" . $row["marca"] . " " . $row['modello'] ."</option>";
-                }
-                else{
-                    $veicoli .= "<option value='" . $row["marca"] . "-" . $row['modello'] . "'>" . $row["marca"] . " " . $row['modello'] ."</option>";
-                }
-                
+    $formORbuttonsHTML = $formHTML;
+    $test_driveHTML = str_replace("[formORbuttons]", $formORbuttonsHTML, $test_driveHTML);
+
+    //ESECUZIONE DELLA QUERY
+    try{
+        $a = array();
+        $db = new DBConnection();
+        $ris = $db->getFilteredVehicles($a);
+        $db->closeConnection();
+        unset($db);
+        $veicoli = '';
+        //INSERIMENTO DEI VEICOLI NEL SELECT
+        foreach($ris as $row){
+            $val = $row["id"] . "-" . $row["marca"] . "-" . $row['modello'];
+            //REIMPOSTO IL VALORE SETTATO
+            if($_POST['test_drive_select_auto'] == $val){
+                $veicoli .= "<option value='" . $row["id"] . "-" . $row["marca"] . "-" . $row['modello'] . "' selected>" . $row["id"] . " - " . $row["marca"] . " " . $row['modello'] ."</option>";
             }
-            $test_driveHTML = str_replace("[veicoli]", $veicoli, $test_driveHTML);
+            else{
+                $veicoli .= "<option value='" . $row["id"] . "-" . $row["marca"] . "-" . $row['modello'] . "'>" . $row["id"] . " - " . $row["marca"] . " " . $row['modello'] ."</option>";
+            }
+            
         }
-        catch(Exception $e){
-            header("location: 500.html");
-            exit();
-        }
+        $test_driveHTML = str_replace("[veicoli]", $veicoli, $test_driveHTML);
+    }
+    catch(Exception $e){
+        header("location: 500.html");
+        exit();
+    }
 
+    if(empty($_POST['test_drive_date']) || empty($_POST['test_drive_select_auto'])){
         $err = $err . "<p>Devi compilare tutti i campi.</p>";
         $test_driveHTML = str_replace("[err]", $err, $test_driveHTML);
 		$test_driveHTML = ripristinoInput($test_driveHTML);
@@ -102,9 +103,18 @@ if(isset($_POST['test_drive_select_auto']) && isset($_POST['test_drive_date'])){
         if (!preg_match("/^[A-Za-z0-9\-]+$/", $_POST["test_drive_select_auto"])) {
 			$err = $err . "<p>Auto non valida</p>";
 		}
+        $_POST["test_drive_date"] = str_replace("T", ' ', $_POST["test_drive_date"]) . ":00";
+        if (strtotime($_POST["test_drive_date"]) < strtotime(date("Y-m-d H:i:s")) || !preg_match('/^[0-9\-\:\s]+$/', $_POST["test_drive_date"])) {
+			$err = $err . "<p>La data non è valida.</p>";
+		}
         //controllo errori
         //esecuzione query controllo se auto esiste
         //far comparire una scritta verde 'richiesta inviata con successo e un link a area personale'
+
+
+        $test_driveHTML = str_replace("[err]", $err, $test_driveHTML);
+		$test_driveHTML = ripristinoInput($test_driveHTML);
+        echo $test_driveHTML;
     }
 
 
@@ -128,7 +138,7 @@ else{
             $veicoli = '';
             //INSERIMENTO DEI VEICOLI NEL SELECT
             foreach($ris as $row){
-                $veicoli .= "<option value='" . $row["marca"] . "-" . $row['modello'] . "'>" . $row["marca"] . " " . $row['modello'] ."</option>";
+                $veicoli .= "<option value='" . $row["id"] . "-" . $row["marca"] . "-" . $row['modello'] . "'>" . $row["id"] . " - " . $row["marca"] . " " . $row['modello'] ."</option>";
             }
             $test_driveHTML = str_replace("[veicoli]", $veicoli, $test_driveHTML);
         }
