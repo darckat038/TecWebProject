6f10522ed6b2c4f780c0dbcc8a164782b8cebaef
EM123AR <emanueleartusi03@gmail.com>
2025-01-14 17:53:32 +0100

    auto.php ver1

--- LuzzAuto/auto.php
+++ LuzzAuto/auto.php
@@ -12,60 +12,61 @@ try {
 
     if ($dettagli != -1) {
         $stringaDettagli = '
-        <div>
-            <dd>Anno</dd>
-            <dt>' . $dettagli['anno'] . '</dt>
-        </div>
-        <div>
-            <dd>Chilometraggio</dd>
-            <dt>' . $dettagli['chilometraggio'] . 'km</dt>
-        </div>
-        <div>
-            <dd>Colore</dd>
-            <dt>Blu chiaro</dt>
-        </div>
-        <div>
-            <dd>Alimentazione</dd>
-            <dt>Elettrico</dt>
-        </div>
-        <div>
-            <dd>Cambio</dd>
-            <dt>Automatico</dt>
-        </div>
-        <div>
-            <dd>Trazione</dd>
-            <dt>Integrale</dt>
-        </div>
-        <div>
-            <dd>Potenza</dd>
-            <dt>490 CV</dt>
-        </div>
-        <div>
-            <dd>Peso</dd>
-            <dt>1765 kg</dt>
-        </div>
-        <div>
-            <dd>Neopatentati abilitati</dd>
-            <dt>No</dt>
-        </div>
-        <div>
-            <dd>Posti</dd>
-            <dt>5</dt>
-        </div>';
+        <h2>Dettagli</h2>
+        <dl>
+            <div>
+                <dd>Anno</dd>
+                <dt>' . $dettagli['anno'] . '</dt>
+            </div>
+            <div>
+                <dd>Chilometraggio</dd>
+                <dt>' . $dettagli['chilometraggio'] . ' km</dt>
+            </div>
+            <div>
+                <dd>Colore</dd>
+                <dt>' . $dettagli['colore'] . '</dt>
+            </div>
+            <div>
+                <dd>Alimentazione</dd>
+                <dt>' . $dettagli['alimentazione'] . '</dt>
+            </div>
+            <div>
+                <dd>Cambio</dd>
+                <dt>' . $dettagli['cambio'] . '</dt>
+            </div>
+            <div>
+                <dd>Trazione</dd>
+                <dt>' . $dettagli['trazione'] . '</dt>
+            </div>
+            <div>
+                <dd>Potenza</dd>
+                <dt>' . $dettagli['potenza'] . ' CV</dt>
+            </div>
+            <div>
+                <dd>Peso</dd>
+                <dt>' . $dettagli['peso'] . ' kg</dt>
+            </div>
+            <div>
+                <dd>Neopatentati abilitati</dd>
+                <dt>' . (($dettagli['neopatentati']==1)?'Sì':'No') . '</dt>
+            </div>
+            <div>
+                <dd>Posti</dd>
+                <dt>' . $dettagli['numeroPosti'] . '</dt>
+            </div>
+        </dl>';
 
     } else {
         $stringaDettagli = "
-            <h3>Qualcuno potrebbe aver portato il veicolo all'autolavaggio</h3>
+            <h2>Qualcuno potrebbe aver portato il veicolo all'autolavaggio</h2>
             <p>Non siamo riusciti a trovare i dettagli del veicolo specificato.</p>
             ";
     }
-    echo $dettagli;
-    echo str_replace("[dettagli_auto]", $stringaDettagli, $autoHTML);
 
+    echo str_replace(search: "[dettagli_auto]", replace: $stringaDettagli, subject: $autoHTML);
 } catch(Exception){
     header("location: 500.html");
     exit();
 }
 
-?>                       
-                            
+?>
