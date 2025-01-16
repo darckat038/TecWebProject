b35c93eb83b92faf0537329a7bd54a36c0773225
EM123AR <emanueleartusi03@gmail.com>
2025-01-16 10:04:18 +0100

    Revert "Separazione toggle.js"
    This reverts commit db58e35d66a107db75c69635530e81b9526811af.

--- LuzzAuto/assets/js/toggle.js
+++ LuzzAuto/assets/js/toggle.js
@@ -7,6 +7,26 @@ document.addEventListener("DOMContentLoaded", function () {
     switchToggle();
 });
 
+
+/*
+* Assicuriamo il funzionamento dell'espansione del filtro da mobile e la corretta gestione del aria-label
+*/
+function switchToggle() {
+
+    let FilterMenu = document.getElementById("list_toggle_button");
+
+    FilterMenu.addEventListener("click", function() {
+        const toggleButton = document.getElementById('list_toggle_button');
+        const toggleInput = document.getElementById('list_toggle_filter');
+        const filterForm = document.getElementById('list_filter_form');
+
+        toggleInput.checked = !toggleInput.checked;
+        toggleButton.setAttribute('aria-expanded', toggleInput.checked.toString());
+        toggleButton.setAttribute('aria-label', toggleInput.checked ? 'Seleziona per comprimere il form filtro' : 'Seleziona per espandere il form filtro');
+    });   
+}
+
+
 /*
 * Utilizzato per riportare il menu ad hamburger nello stato in cui lo si è lasciato se si cambia viewport
 */
