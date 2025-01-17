4732c5ae524af125c1d7045ddd2b22b5ce79c86a
Filippo Bellon <filippo.uni.bellon03@gmail.com>
2025-01-17 16:21:51 +0100

    aggiunto file js controlli listino

---
+++ LuzzAuto/assets/js/form_validation_listino.js
@@ -0,0 +1,83 @@
+document.addEventListener("DOMContentLoaded", function () {
+	validateFastSearch();
+});
+
+function validateFastSearch() {
+	let form = document.getElementById("list_filter_form");
+
+	form.addEventListener("submit", function(event) {
+
+		var ok = true;
+		let msg = "";
+
+		console.log("submit event");
+		resetFormError();
+		if(!validateMarca()) {
+			ok = false;
+			msg += "<p tabindex=\"0\" id=\"marca_err\">Marca non valida, puoi usare solo lettere, numeri, spazi(non all'inizio e alla fine) e il carattere \"-\".</p>";
+		}
+		if(!validateModello()) {
+			ok = false;
+			msg += "<p id=\"modello_err\">Modello non valido, puoi usare solo lettere, numeri, spazi(non all'inizio e alla fine) e il carattere \"-\".</p>";
+		}
+		if(!validatePrezzo()) {
+			ok = false;
+			msg += "<p id=\"prezzoMax_err\">Prezzo non valido, inserisci un prezzo maggiore di 0.</p>";;
+		}
+		if(!validateCondizione()) {
+			ok = false;
+			msg += "<p id=\"prezzoMax_err\">Prezzo non valido, inserisci un prezzo maggiore di 0.</p>";;
+		}
+		if(!ok) {
+			console.log("Prevenzione del submit, errore trovato.");
+			addFormError(msg);
+			event.preventDefault();
+		} else {
+			console.log("Tutto ok, il form verrà inviato.");
+		}
+	});
+}
+
+/*
+* Controllo la marca del veicolo
+*/
+function validateMarca() {
+	const allowedChars = /^([A-Za-z0-9\-]+( [A-Za-z0-9\-]+)*)?$/; // lettere maiuscole e minuscole, numeri e il carattere trattino(-)
+
+	var marca = document.getElementById("list_filter_marca").value;
+
+	return allowedChars.test(marca);
+}
+
+/*
+* Controllo il modello del veicolo
+*/
+function validateModello() {
+	const allowedChars = /^([A-Za-z0-9\-]+( [A-Za-z0-9\-]+)*)?$/; // lettere maiuscole e minuscole, numeri e il carattere trattino(-)
+
+	var modello = document.getElementById("list_filter_modello").value;
+
+	return allowedChars.test(modello);
+}
+
+/*
+* Controllo il modello del veicolo
+*/
+function validateCondizione() {
+	const allowedChars = /^([A-Za-z0-9]+( [A-Za-z0-9]+)*)?$/; // lettere maiuscole e minuscole, numeri
+
+	var condizione = document.getElementById("list_filter_condizione").value;
+
+	return allowedChars.test(condizione);
+}
+
+/*
+* Controllo il prezzo massimo inserito
+*/
+function validatePrezzo() {
+	var prezzoMax = document.getElementById("list_filter_prezzoMax").value;
+
+	console.log(prezzoMax);
+	
+	return (prezzoMax != "" ? prezzoMax > 0 : true);
+}
