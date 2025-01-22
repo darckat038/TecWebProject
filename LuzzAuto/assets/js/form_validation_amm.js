930f99bff0c400e5fcd3fa219c3893362fbe2b6b
Filippo Bellon <filippo.games03@gmail.com>
2025-01-22 21:37:15 +0100

    aggiunti controlli js form gestione prenotazione admin

--- LuzzAuto/assets/js/form_validation_amm.js
+++ LuzzAuto/assets/js/form_validation_amm.js
@@ -1,6 +1,7 @@
 //INCLUDERE NELLA PAGINA HTML IL VALIDATION TOOL
 window.addEventListener('load', function () {
 	validateFormEliminaAuto();
+    validateFormGestPrenotazione();
 });
 
 function isNumber(value){
@@ -23,10 +24,12 @@ function validateFormEliminaAuto(){
             div.innerHTML = "";
             let msg = "";
             let ok = true;
+            /*
             if(!validateAuto()){
                 msg+="<p>L'auto non è valida.</p>";
                 ok = false;
             }
+                */
             if(!validateEmptyAuto()){
                 msg+="<p>Selezionare un'auto da eliminare.</p>";
                 ok = false;
@@ -56,3 +59,45 @@ function validateAuto(){
     return true;
 }
 
+function validateFormGestPrenotazione(){
+    let form = document.getElementById('gestionePrenotazioneFormAdmin');
+    if(form){
+        form.addEventListener('submit', function (event){
+            resetFormError(0);
+
+            let msg = "";
+            let ok = true;
+
+            if(!validatePrenotazione()){
+                msg += "<p>Prenotazione selezionata non valida</p>";
+                ok = false;
+            }
+            if(!validateAzione()) {
+                msg += "<p>Azione selezionata non valida</p>";
+                ok = false;
+            }
+            if(!ok){
+                addFormError(msg, 0);
+                event.preventDefault();
+            }
+            
+        });
+    }
+}
+
+function validatePrenotazione(){
+    var prenotazione = document.getElementById("gestPrenAdmin").value;
+    if(prenotazione != "" && isNumber(prenotazione)){
+        return true;
+    }
+    return false;
+}
+
+function validateAzione(){
+    var azione = document.getElementById("azioneAdmin").value;
+    console.log(azione);
+    if(azione == "accetta" || azione == "rifiuta"){
+        return true;
+    }
+    return false;
+}
