ab0dfeb4b7d98e1c0197d95e6cbff9daa1dcbb10
FilippoDiviesti <filippo.diviesti@studenti.unipd.it>
2025-01-22 14:04:55 +0100

    validate eliminazione auto js

---
+++ LuzzAuto/assets/js/form_validation_amm_elimina.js
@@ -0,0 +1,62 @@
+//INCLUDERE NELLA PAGINA HTML IL VALIDATION TOOL
+window.addEventListener('load', function () {
+	validateFormEliminaAuto();
+});
+
+function isNumber(value){
+    try{
+        if(isNaN(value)){
+            return false;
+        }
+        return true;
+    }
+    catch(error){
+        console.log("An error occurred: ", error.message);
+    }
+}
+
+function validateFormEliminaAuto(){
+    let form = document.getElementById('eliminaAutoFormAdmin');
+    if(form){
+        form.addEventListener('submit', function (event){
+            let div = document.getElementsByClassName("form_errors")[2];
+            div.innerHTML = "";
+
+            let msg = "";
+            let ok = true;
+            
+            if(!validateAuto()){
+                msg+="<p>L'auto non è valida js.</p>";
+                ok = false;
+            }
+    
+            if(!validateEmptyAuto()){
+                msg+="<p>Selezionare un'auto da eliminare js.</p>";
+                ok = false;
+            }
+    
+            if(!ok){
+                div.innerHTML += msg;
+                event.preventDefault();
+            }
+        });
+    }
+}
+
+function validateEmptyAuto(){
+    var auto = document.getElementById("eliminaAutoAdmin").value;
+    if(auto == ""){
+        return false;
+    }
+    return true;
+}
+
+function validateAuto(){
+    var auto = document.getElementById("eliminaAutoAdmin").value;
+    const validChars = /^[A-Za-z0-9\-\s]+$/;
+    if(auto.split("-")[0] != "" &&  (!validChars.test(auto) || !isNumber(auto.split("-")[0]))){
+        return false;
+    }
+    return true;
+}
+
