e318a84d0b19884855c5cb7a524ebcb26660dcd3
EM123AR <emanueleartusi03@gmail.com>
2025-01-22 16:57:04 +0100

    Megafix menù hamburger

--- LuzzAuto/assets/js/toggle.js
+++ LuzzAuto/assets/js/toggle.js
@@ -1,79 +1,68 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Evento resize sulla finestra
+    // Gestione menù ad hamburger
     adjustMenuOnResize();
-    // Evento click su menu hamburger
-    menuOpenClose();
     // Evento click filtro listino
     switchToggle();
 });
-
-
-
 /*
-* Utilizzato per riportare il menu ad hamburger nello stato in cui lo si è lasciato se si cambia viewport
+* Gestione menù ad hamburger
 */
 function adjustMenuOnResize() {
-
-    window.addEventListener("resize", function() {
-
-        var x = document.getElementById("menu");
-        var y = document.getElementById("menu-button-container");
-
-        if (window.innerWidth > 1024) {
-            x.style.display = "flex";                       // Passaggio a modalità desktop
+    let menu = document.getElementById("menu");
+    let tasto = document.getElementById("menu-button-container");
+    let links = menu.querySelectorAll("a");
+    function menuOpenClose() {
+        // Cambia tra menù visibile e nascosto
+        if (tasto.classList.contains("change")) {
+            menu.style.position = "static";
+            menu.style.margin = "0";
+            if (window.innerWidth > 1024) menu.style.marginLeft = "auto";
+            else menu.style.marginTop = "3.5em";
+            menu.setAttribute("tabindex","0");
+            links.forEach(link => {
+                link.setAttribute("tabindex", "0");
+                link.style.pointerEvents = "auto";
+            });
+            tasto.setAttribute("aria-label","Seleziona per comprimere il menù di navigazione");
         } else {
-            if (x.style.display === "flex" && !(y.classList.contains("change"))) {
-                x.style.display = "none";                   // Passaggio da desktop a mobile
-            }
-                                                            // Passaggio da mobile a mobile
+            menu.style.position = "absolute";
+            menu.style.margin = "-9999em";
+            menu.setAttribute("tabindex","-1");
+            links.forEach(link => {
+                link.setAttribute("tabindex", "-1");
+                link.style.pointerEvents = "none";
+            });
+            tasto.setAttribute("aria-label","Seleziona per espandere il menù di navigazione");
         }
+    }
+    // Esegui al caricamento e al resize
+    menuOpenClose();
+    window.addEventListener('resize', function(){
+        if (window.innerWidth > 1024) tasto.classList.add("change");
+        else tasto.classList.remove("change");
+        menuOpenClose();
+    });
+    tasto.addEventListener('click', function() {
+        // Cambia tra linee e croce
+        tasto.classList.toggle("change");   
+        menuOpenClose();
     });
 }
-
-
-/*
-* Utilzzato per animazione menu hamburger
-*/
-function menuOpenClose() {
-
-    let HamMenu = document.getElementById("menu-button-container");
-
-    HamMenu.addEventListener("click", function() {
-        let y = document.getElementById("menu-button-container");
-        let x = document.getElementById("menu");
-
-        if (x.style.display === "flex") {
-            x.style.display = "none";
-        } else {
-            x.style.display = "flex";
-        }
-        y.classList.toggle("change");
-    });
-}
-
-
-
 /*
 * Assicuriamo il funzionamento dell'espansione del filtro da mobile e la corretta gestione del aria-label
 */
 function switchToggle() {
-
     let toggleButton = document.getElementById('list_toggle_button');
-
-    if(toggleButton){
-        
+    if(toggleButton){ 
         let toggleInput = document.getElementById('list_toggle_filter');
         let filterForm = document.getElementById('list_filter_form');
         let inputs = filterForm.querySelectorAll("input, select");   
-        
-        
         function filterOpenClose() {
             // Controlla toggle
             if (toggleInput.checked) {
                 // Mostra menù
                 filterForm.style.position = "static";
                 filterForm.style.margin = "0";
-
                 // Mostra toggle se su mobile altrimenti rimuovi
                 if(window.innerWidth <= 1024) {
                     toggleButton.style.position = "static";
@@ -84,7 +73,6 @@ function switchToggle() {
                     toggleButton.style.margin = "-9999em";
                     toggleButton.setAttribute("tabindex", "-1");
                 }
-
                 // Mostra campi input
                 inputs.forEach(input => {
                     input.setAttribute("tabindex", "0");
@@ -95,9 +83,7 @@ function switchToggle() {
                 // Rimuovi menù
                 filterForm.style.position = "absolute";
                 filterForm.style.margin = "-9999em";
-
                 // Rimuovi toggle non serve perché su mobile
-
                 // Rimuovi campi input
                 inputs.forEach(input => {
                     input.setAttribute("tabindex", "-1");
@@ -106,29 +92,21 @@ function switchToggle() {
                 });
             }
         }
-
         // Ascolta pressioni toggle
         toggleButton.addEventListener("click", function() {
             // Cambio stato ed espansione/riduzione menù
             toggleInput.checked = !toggleInput.checked;
             filterOpenClose();
-
             // Impostazione ARIA attributes
             toggleButton.setAttribute('aria-expanded', toggleInput.checked.toString());
             toggleButton.setAttribute('aria-label', toggleInput.checked ? 'Seleziona per comprimere il form filtro' : 'Seleziona per espandere il form filtro');
-
-            });
-
+        });
         // Esegui al caricamento e al resize
+        toggleInput.checked = true;
+        filterOpenClose();
         window.addEventListener('resize', function() {
             toggleInput.checked = true;
             filterOpenClose();
         });
-        window.addEventListener('DOMContentLoaded', function() {
-            toggleInput.checked = true;
-            filterOpenClose();
-        });
-
     }
-
 }
