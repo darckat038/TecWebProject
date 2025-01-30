5914d9fd7672983bf6f76a3666b32a2f40752e1e
Filippo Bellon <filippo.games03@gmail.com>
2025-01-30 18:40:20 +0100

    modificato back to origin js

--- LuzzAuto/assets/js/back_to_origin.js
+++ LuzzAuto/assets/js/back_to_origin.js
@@ -7,8 +7,8 @@ function setBackToOriginTestDriveLogin() {
     let linkLogin = document.getElementById('test_drive_login_link');
     if(linkLogin){
         linkLogin.addEventListener('click', function (event){
-            var c = "backToOrigin=test_drive.php#test_drive_prenota";
-            document.cookie = c;
+            var cL = "backToOrigin=test_drive.php#test_drive_prenota";
+            document.cookie = cL;
         });
     }
 }
@@ -17,8 +17,8 @@ function setBackToOriginTestDriveSignup() {
     let linkSignup = document.getElementById('test_drive_signup_link');
     if(linkSignup){
         linkSignup.addEventListener('click', function (event){
-            var c = "backToOrigin=test_drive.php#test_drive_prenota";
-            document.cookie = c;
+            var cS = "backToOrigin=test_drive.php#test_drive_prenota";
+            document.cookie = cS;
         });
     }
 }
